ce475e0c46921bc10c005dcb6d62b70619f820c0
Gabriel Brunichaki <gabriel.brunichaki@triider.com>
2020-10-03 19:48:11 -0300

    pages template

---
+++ src/template/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: #e5e5e5;
+`
+
+export const Header = styled.header`
+  height: 20vh;
+  min-height: 300px;
+  background: #017cba;
+`
+
+export const Content = styled.main`
+  height: 80vh;
+  margin: -100px 16px 25px;
+  padding: 24px 16px;
+  border-radius: 8px;
+  border: 1px solid #e6e6f0;
+  background: #fff;
+`
