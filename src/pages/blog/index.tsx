d4811ee47ca4016ca6d52257871186b3fb26af11
marceloAzevedo <marcelo@triider.com>
2020-10-03 21:11:33 -0300

    adding page blog and fonts

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+import { Container, Card } from './styles'
+import primeiroBlog from '../../assets/primeiroBlog.jpeg'
+import segundoBlog from '../../assets/segundoBlog.jpeg'
+import terceiroBlog from '../../assets/terceiroBlog.png'
+import quartoBlog from '../../assets/quartoBlog.jpeg'
+
+import Template from '../../template'
+
+const blog: React.FC = () => {
+  return (
+    <Template title="Blog">
+      <Container>
+        <Card>
+          <img src={primeiroBlog} />
+          <div>
+            <h3>Depressão</h3>
+            <p>A Depressão é uma doença que afeta o humor do indivíduo...</p>
+            <a href="http://fernandapsicologa.com.br/textos/depressao.html">
+              Ler mais...
+            </a>
+          </div>
+        </Card>
+        <Card>
+          <img src={segundoBlog} />
+          <div>
+            <h3>Suicídio? Melhor Prevenir!</h3>
+            <p>Há um consenso científico em todas as áreas da saúde...</p>
+
+            <a href="https://blog.psicologiaviva.com.br/prevenir-suicidio/">
+              Ler mais...
+            </a>
+          </div>
+        </Card>
+        <Card>
+          <img src={terceiroBlog} />
+          <div>
+            <h3>Esgotamento Psicológico</h3>
+            <p>
+              O esgotamento psicológico é a sensação de exaustão mental e
+              física...
+            </p>
+            <a href="https://www.vittude.com/blog/o-que-causa-esgotamento-psicologico/">
+              Ler mais...
+            </a>
+          </div>
+        </Card>
+        <Card>
+          <img src={quartoBlog} />
+          <div>
+            <h3>Fim do isolamento</h3>
+            <p>
+              A maioria das pessoas já deve ter se perguntado quando acaba a
+              quarentena...
+            </p>
+            <a
+              href={
+                'https://www.vittude.com/blog/quando-acaba-a-quarentena-dicas-para-se-preparar/'
+              }
+            >
+              Ler mais...
+            </a>
+          </div>
+        </Card>
+      </Container>
+    </Template>
+  )
+}
+
+export default blog
---
+++ src/pages/blog/styles.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  /* height: 100%; */
+  flex: 1;
+`
+
+export const Card = styled.div`
+  display: flex;
+  width: 100%;
+  height: 110px;
+  border: 1px solid #eeeeee;
+  border-radius: 10px;
+  padding: 12px;
+  margin-bottom: 24px;
+
+  :hover {
+    opacity: 0.8;
+  }
+
+  img {
+    border-radius: 10px;
+    height: 86px;
+    width: 90px;
+    margin-right: 20px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    width: 100%;
+
+    h3 {
+      font-family: 'Poppins', sans-serif;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 14px;
+      line-height: 24px;
+      /* or 171% */
+
+      display: flex;
+      align-items: center;
+
+      color: #000000;
+    }
+
+    p {
+      font-family: 'Poppins', sans-serif;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 10px;
+      line-height: 20px;
+
+      color: #bfbfbf;
+    }
+
+    a {
+      font-family: 'Poppins', sans-serif;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 11px;
+      line-height: 24px;
+      text-decoration-line: underline;
+
+      color: #017cba;
+      text-align: right;
+    }
+  }
+`
