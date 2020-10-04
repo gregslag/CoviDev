11e0d340fc26f84c2de02d90e1716e585447c69a
Gregory Lagranha <gregslag@MacBook-Pro.local>
2020-10-03 23:53:15 -0300

    ignore types after build

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ const withImages = require('next-images')
 
 module.exports = withImages({
   esModule: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 })
